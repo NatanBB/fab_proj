94b1a6b93ed9564ba4b0ecb8b7c9d499cb52e986
NatanBB <natanborba77@gmail.com>
2020-03-04 13:35:34 -0300

    Apresntacao app core

--- tutorialpress/core/models.py
+++ tutorialpress/core/models.py
@@ -1,3 +1,9 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+
+class Publicacao(models.Model):
+    titulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    is_publicada = models.BooleanField(default=False)
